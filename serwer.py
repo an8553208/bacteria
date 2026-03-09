fd436eec99a39cb110f8dce6beba672d19641974
Я <an8553208@gmail.com>
2026-03-09 19:57:02 +0300

    урок1

---
+++ serwer.py
@@ -0,0 +1,8 @@
+import socket
+import time
+main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+main_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
+main_socket.bind(('localhost',10000))
+main_socket.setblocking(False)
+main_socket.listen(5)
+print ('socket создан')
