f86be472164344c3293f16db2376ea43ec9f5f36
Я <an8553208@gmail.com>
2026-03-30 19:21:28 +0300

    дз3

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
 import random
 import pygame
+import math
 main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 main_socket.connect(('localhost',10000))
 pygame.init()
+radius = 50
 width = 800
 height = 600
 CC = (width//2,height//2)
@@ -20,10 +22,17 @@ while run:
     if pygame.mouse.get_focused():
         pos = pygame.mouse.get_pos()
         vector = (pos[0]-CC[0],pos[1]-CC[1])
-        if old != vector:
+        lenv = math.sqrt(vector[0]**2 + vector[1]**2)
+        vector = (vector[0]/lenv,vector[1]/lenv)
+        if lenv < radius:
+            vector = (0,0)
+        if vector != old:
             old = vector
-            msg = ('')
-    main_socket.send(f'{random.randint(1,9)}'.encode())
+            msg = f"<{vector[0]},{vector[1]}>"
+            main_socket.send(msg.encode())
     data = main_socket.recv(1024).decode()
     print (data)
+    screen.fill('green')
+    pygame.draw.circle(screen, (255, 0, 0), CC, radius)
+    pygame.display.update()
 
