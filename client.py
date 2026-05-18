376fcec6c70d4fb41be6d9b457f1d2fd0e0c39ae
Я <an8553208@gmail.com>
2026-05-18 18:59:56 +0300

    ДЗ 7

--- client.py
+++ client.py
@@ -2,37 +2,53 @@ import socket
 import random
 import pygame
 import math
-main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-main_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
-main_socket.connect(('localhost',10000))
+
+main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+main_socket.connect(('localhost', 10000))
 pygame.init()
 width = 800
 height = 600
-CC = (width//2,height//2)
-old = (0,0)
+CC = (width // 2, height // 2)
+old = (0, 0)
 radius = 10
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('бактерия')
 run = True
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
     if pygame.mouse.get_focused():
         pos = pygame.mouse.get_pos()
-        vector = (pos[0]-CC[0],pos[1]-CC[1])
+        vector = (pos[0] - CC[0], pos[1] - CC[1])
         lenv = math.sqrt(vector[0]**2 + vector[1]**2)
-        if lenv!= 0:
-            vector = (vector[0]/lenv,vector[1]/lenv)
+        if lenv != 0:
+            vector = (vector[0] / lenv, vector[1] / lenv)
         if lenv < radius:
-            vector = (0,0)
+            vector = (0, 0)
         if vector != old:
             old = vector
-            msg = f"<{vector[0]},{vector[1]}>"
+            msg = f"<{vector[0]:.3f},{vector[1]:.3f}>"
             main_socket.send(msg.encode())
-    data = main_socket.recv(1024).decode()
-    print (data)
+
+    try:
+        main_socket.settimeout(0.1)
+        data = main_socket.recv(1024).decode()
+        if data:
+            print(data)
+    except socket.timeout:
+        pass
+    except ConnectionResetError:
+        run = False
+    except Exception:
+        run = False
+
     screen.fill('darkgreen')
     pygame.draw.circle(screen, (255, 0, 0), CC, radius)
     pygame.display.update()
 
+main_socket.close()
+pygame.quit()
--- serwer.py
+++ serwer.py
@@ -1,133 +1,157 @@
 import socket
 import time
 import psycopg2
-from sqlalchemy import create_engine,Column,Integer,String
-from sqlalchemy.orm import declarative_base,sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
 import faker
 import random
 import pygame
 
-
-
 engine = create_engine("postgresql+psycopg2://postgres:228228765@localhost/PYTHON")
 Base = declarative_base()
+
 class Players(Base):
     __tablename__ = 'Players'
-    id = Column(Integer,primary_key=True,autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     adres = Column(String)
-    x = Column(Integer,default=500)
-    y = Column(Integer,default=500)
-    size = Column(Integer,default=10)
-    errors = Column(Integer,default=0)
-    ABSspeed = Column(Integer,default=1)
-    speedx = Column(Integer,default=0)
-    speedy = Column(Integer,default=0)
+    x = Column(Integer, default=500)
+    y = Column(Integer, default=500)
+    size = Column(Integer, default=10)
+    errors = Column(Integer, default=0)
+    ABSspeed = Column(Integer, default=1)
+    speedx = Column(Integer, default=0)
+    speedy = Column(Integer, default=0)
+
     def __init__(self, name, adres):
         self.name = name
         self.adres = adres
+
 class LocalPlayer():
-    def __init__(self,id,name,sock,adres):
+    def __init__(self, id, name, sock, adres):
         self.id = id
-        self.name=name
-        self.sock=sock
-        self.adres=adres
-        self.DB = s.get(Players,self.id)
-        self.x=500
-        self.y=500
-        self.size=10
-        self.errors=0
-        self.ABSspeed=1
-        self.speedx=0
-        self.speedy=0
+        self.name = name
+        self.sock = sock
+        self.adres = adres
+        self.DB = s.get(Players, self.id)
+        self.x = 500
+        self.y = 500
+        self.size = 10
+        self.errors = 0
+        self.ABSspeed = 1
+        self.speedx = 0
+        self.speedy = 0
+
     def update(self):
-        self.x+=self.speedx
-        self.y+=self.speedy
-    def change_speed(self,vector):
+        self.x += self.speedx
+        self.y += self.speedy
+        if self.DB:
+            self.DB.x = self.x
+            self.DB.y = self.y
+            s.merge(self.DB)
+            s.commit()
+
+    def change_speed(self, vector):
         vector = find(vector)
-        if vector[0]==0 and vector[1]==0:
-            self.speedx=0
-            self.speedy=0
+        if vector[0] == 0 and vector[1] == 0:
+            self.speedx = 0
+            self.speedy = 0
         else:
-            vector=vector[0]*self.ABSspeed,vector[1]*self.ABSspeed
-            self.speedx=vector[0]
-            self.speedy=vector[1]
+            vector = vector[0] * self.ABSspeed, vector[1] * self.ABSspeed
+            self.speedx = int(vector[0])
+            self.speedy = int(vector[1])
+        if self.DB:
+            self.DB.speedx = self.speedx
+            self.DB.speedy = self.speedy
+            s.merge(self.DB)
+            s.commit()
 
 def find(vector):
     start = vector.find('<')
     end = vector.find('>')
     if start < end and start != -1:
-        data = vector[start+1:end]
+        data = vector[start + 1:end]
         data = data.split(',')
-        data = list(map(float,data))
+        data = list(map(float, data))
         return data
-    return ''
+    return [0, 0]
+
 Base.metadata.create_all(engine)
 Session = sessionmaker(engine)
 s = Session()
+
 pygame.init()
-width_room=5000
-height_room=5000
-width_server=500
-height_server=500
-screen = pygame.display.set_mode((width_server,height_server))
+width_room = 5000
+height_room = 5000
+width_server = 500
+height_server = 500
+screen = pygame.display.set_mode((width_server, height_server))
 pygame.display.set_caption("SERVER")
 clok = pygame.time.Clock()
 Fps = 180
-main_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-main_socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
-main_socket.bind(('localhost',10000))
+
+main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+main_socket.bind(('localhost', 10000))
 main_socket.setblocking(False)
 main_socket.listen(5)
-print ('socket создан')
-plaers = { }
+print('socket создан')
+players = {}
 run = True
+
 while run:
     clok.tick(Fps)
     try:
         new_socket, addr = main_socket.accept()
         print('Подключился', addr)
         new_socket.setblocking(False)
-        player = Players('name',addr)
-        s.merge(player)
+        addr_str = f'({addr[0]},{addr[1]})'
+        player_db = Players(name=f'Player_{len(players) + 1}', adres=addr_str)
+        s.add(player_db)
         s.commit()
-        addr=f'({addr[0]},{addr[1]})'
-        data = s.query(Players).filter(Players.adres==addr)
-        for user in data:
-            localplayer = LocalPlayer(user.id,user.name,new_socket,addr)
-            plaers[user.id] = localplayer
+        local_player = LocalPlayer(player_db.id, player_db.name, new_socket, addr_str)
+        local_player.DB = player_db
+        players[player_db.id] = local_player
     except BlockingIOError:
         pass
-    for id in list(plaers):
+
+    for player_id in list(players.keys()):
         try:
-            data = plaers[id].sock.recv(1024).decode()
-            print (data)
-            plaers[id].change_speed(data)
-        except:
-            pass   
-    for id in list(plaers):
-        try:
-            plaers[id].sock.send("1".encode())
-        except: 
-            plaers[id].sock.close()
-            del plaers[id]
-            s.query(Players).filter(Players.id==id).delete()
+            data = players[player_id].sock.recv(1024).decode()
+            if data:
+                print(data)
+                players[player_id].change_speed(data)
+        except (BlockingIOError, ConnectionResetError, OSError):
+            players[player_id].sock.close()
+            s.query(Players).filter(Players.id == player_id).delete()
             s.commit()
+            del players[player_id]
+
+    for player_id in list(players.keys()):
+        try:
+            players[player_id].sock.send("1".encode())
+        except (ConnectionResetError, OSError):
+            players[player_id].sock.close()
+            s.query(Players).filter(Players.id == player_id).delete()
+            s.commit()
+            del players[player_id]
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    screen.fill('darkgreen')
-    for id in list(plaers): 
-        player = plaers[id]
-        x = player.x*width_server//width_room
-        y = player.y*width_server//width_room
-        size = player.size*width_server//width_room
-        pygame.draw.circle(screen,'red',(x,y),size)
 
-    for id in list (plaers):
-        plaers[id].update()
-        
-    
+    screen.fill('darkgreen')
+    for player_id in players:
+        player = players[player_id]
+        x = player.x * width_server // width_room
+        y = player.y * width_server // width_room
+        size = player.size * width_server // width_room
+        pygame.draw.circle(screen, 'red', (x, y), size)
+
+    for player_id in list(players.keys()):
+        players[player_id].update()
+
     pygame.display.update()
 
-
+s.close()
+main_socket.close()
